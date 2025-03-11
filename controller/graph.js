a289c1be56dafce99b74380c95ddbeb36ae471a0
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2025-03-11 11:27:14 +0100

    Add direction schema and modify API

--- controller/graph.js
+++ controller/graph.js
@@ -1,8 +1,6 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 //const { json } = require('express');
 const prisma = new PrismaClient();
-
-
 
 const uploadGraph = async (req, res) => {
     console.log(req.body);
@@ -10,9 +8,8 @@ const uploadGraph = async (req, res) => {
     try {
         await prisma.edge.deleteMany();
         await prisma.node.deleteMany();
-    }
-    catch (error) {
-        res.status(500).json({ 'error': error });
+    } catch (error) {
+        res.status(500).json({ error: error });
         console.error(error);
     }
     // Then upload the new graph
@@ -20,34 +17,49 @@ const uploadGraph = async (req, res) => {
         const nodes = req.body.nodes;
         const edges = req.body.edges;
         await prisma.node.createMany({
-            data: nodes
+            data: nodes,
         });
         await prisma.edge.createMany({
-            data: edges
+            data: edges,
         });
-        res.json({ message: 'Graph uploaded' });
-    }
-    catch (error) {
-        res.status(500).json({ 'error': error });
+
+        for (let i = 0; i < edges.length; i++) {
+            await prisma.direction.create({
+                data: {
+                    nodeId: edges[i].from,
+                    edgeId: edges[i].id,
+                    //imageUrl: null,
+                },
+            });
+
+            await prisma.direction.create({
+                data: {
+                    nodeId: edges[i].to,
+                    edgeId: edges[i].id,
+                    //imageUrl: null,
+                },
+            });
+        }
+
+        res.json({ message: "Graph uploaded" });
+    } catch (error) {
+        res.status(500).json({ error: error });
         console.error(error);
     }
-}
+};
 
 const downloadGraph = async (req, res) => {
     try {
         const nodes = await prisma.node.findMany();
         const edges = await prisma.edge.findMany();
         res.json({ nodes, edges });
-    }
-    catch (error) {
-        res.status(500).json({ 'error': error });
+    } catch (error) {
+        res.status(500).json({ error: error });
         console.error(error);
     }
-}
-
-
+};
 
 module.exports = {
     uploadGraph,
-    downloadGraph
+    downloadGraph,
 };
