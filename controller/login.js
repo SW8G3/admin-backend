4e8738307b9b4a39c59a27e41274d2b83c982331
CrackerGames <Tubbe.friese@gmail.com>
2025-04-02 12:06:53 +0200

    Refactor user authentication logic, update tests, and configure CI workflows

--- controller/login.js
+++ controller/login.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const login = async (req, res) => {
     try {
         const { username, password } = req.body;
-        const user = await prisma.user.findMany({
+        const user = await prisma.user.findUnique({
             where: {
                 username: username
             }
@@ -16,15 +16,12 @@ const login = async (req, res) => {
         if (!user) {
             return res.status(401).json({ message: 'Invalid username or password' });
         }
-        for (let i = 0; i < user.length; i++) {
-            const isPasswordValid = await bcrypt.compare(password, user[i].password);
-            if (isPasswordValid) {
-                const token = jwt.sign({ id: user[i].id }, process.env.JWT_SECRET, { expiresIn: '24h' });
-                return res.json({ token });
-            }
-
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid username or password' });
         }
-        return res.status(401).json({ message: 'Invalid username or password' });
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ message: 'Login successful', token });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal server error' });
@@ -35,6 +32,17 @@ const login = async (req, res) => {
 const createUser = async (req, res) => {
     try {
         const { username, password } = req.body;
+
+        // Check if the username is already taken
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                username: username
+            }
+        });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Username is already taken' });
+        }
+
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = await prisma.user.create({
             data: {
