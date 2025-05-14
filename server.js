b2f86858888bef730ededbd32564cb792c6f3ba4
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2025-05-14 10:25:52 +0200

    Rename login to auth

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/graph', graphRouter);
-app.use('/login', loginRouter);
+app.use('/auth', loginRouter);
 app.use('/qr', qrRouter);
 
 // Global error handler
